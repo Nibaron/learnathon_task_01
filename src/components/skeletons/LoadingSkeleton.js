a5580a4350fc7744983f51c0dda93a43967dc5ce
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-11-20 17:17:51 +0600

    skeleton background updated

--- src/components/skeletons/LoadingSkeleton.js
+++ src/components/skeletons/LoadingSkeleton.js
@@ -14,19 +14,19 @@ export const LoadingSkeleton = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {/**Skeleton Displaying for 15 card */}
           {[...Array(15)].map((_, index) => (
-            <div className="card-skeleton bg-gray-300 p-4 rounded-lg animate-pulse">
+            <div className="card-skeleton bg-gray-100 p-4 rounded-lg animate-pulse">
               {/**for id number right corner*/}
-              <div className="skeleton-text mb-2 w-1/4 h-4 bg-gray-400"></div>
+              <div className="skeleton-text mb-2 w-1/4 h-4 bg-gray-200"></div>
 
               {/** for center image*/}
-              <div className="skeleton-image mx-auto mb-2 w-32 h-32 bg-gray-400"></div>
+              <div className="skeleton-image mx-auto mb-2 w-32 h-32 bg-gray-200"></div>
 
               {/**Title and Types */}
               <div className="skeleton-type my-2 flex flex-wrap gap-2 justify-center">
-                <div className="skeleton-heading text-xl w-3/4 h-6 bg-gray-400 "></div>
-                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-400"></div>
-                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-400"></div>
-                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-400"></div>
+                <div className="skeleton-heading text-xl w-3/4 h-6 bg-gray-200 "></div>
+                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-200"></div>
+                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-200"></div>
+                <div className="skeleton-subheading text-lg w-1/4 h-4 bg-gray-200"></div>
               </div>
             </div>
           ))}
